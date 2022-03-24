7e8e0dd5971b3b7619ca0fda6412aa3f23a6111b
FaridKishiyev <99797424+FaridKishiyev@users.noreply.github.com>
2022-03-25 01:08:27 +0400

    Currecy enum

---
+++ CurrecyEnum/CurrecyEnum/Enums/Currency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrecyEnum.Enums
+{
+    enum Currency
+    {
+        USD,
+        EURO,
+        TRY
+    }
+}
